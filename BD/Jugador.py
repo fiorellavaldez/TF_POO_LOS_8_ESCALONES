a3aca738900a03328e2ba3e68c7a07cdb5c3e388
kadiruriel <kadiruriel@gmail.com>
2024-11-02 06:31:51 -0300

    Add files via upload

---
+++ BD/Jugador.py
@@ -0,0 +1,6 @@
+class Jugador:
+    def _init_(self, id_usuario, nombre_usuario, puntaje, activo):
+        self.id_usuario = id_usuario
+        self.nombre_usuario = nombre_usuario
+        self.puntaje = puntaje
+        self.activo = activo
