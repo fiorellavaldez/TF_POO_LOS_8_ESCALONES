b317409fb2b3f64e028f6279e503b43b6be1b0af
Fiore <fiorellavald3z@gmail.com>
2024-11-08 20:32:42 -0300

    imports arreglados

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from controlador.ControladorInicio import ControladorInicio
+from controlador.ControladorPantallaInicio import ControladorPantallaInicio
 from PyQt6 import QtWidgets
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    controlador = ControladorInicio()
+    controlador = ControladorPantallaInicio()
     sys.exit(app.exec()) #hola
 
