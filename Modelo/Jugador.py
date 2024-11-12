b27fc2cc623786dc0ab1c2a99f2108b642277b1a
kadiruriel <kadiruriel@gmail.com>
2024-11-11 21:28:41 -0300

    Update Jugador.py
    cambio hechos despues de preguntar al profe en clases

--- Modelo/Jugador.py
+++ Modelo/Jugador.py
@@ -1,8 +1,12 @@
 from .JugadorDAO import JugadorDAO
 
 class Jugador:
-    def __init__(self):
+    def __init__(self, id_jugador, nombre_jugador, puntaje, estado_jugador):
         self.__jugadordao = JugadorDAO()
+        self.__id_jugador = id_jugador
+        self.__nombre_jugador = nombre_jugador
+        self.__puntaje = puntaje
+        self.__estado_jugador = estado_jugador
     
 
     def get_all_jugadores(self):
@@ -12,17 +16,32 @@ class Jugador:
     def get_jugador(self, id_jugador):
         return self.__jugadordao.get_jugador(id_jugador)
 
-    '''
-    def agregar_jugador(self, id_jugador, nombre_jugador, puntaje , estado_jugador):
-        return self.__jugadordao.agregar_jugador(id_jugador, nombre_jugador, puntaje , estado_jugador)
-    '''
-    #nos interesa agregar solo el nombre el ID ya es de tipo SERIAL no es neceario pasar como parametro
-    def agregar_jugador(self, nombre_jugador):
-        return self.__jugadordao.agregar_jugador( nombre_jugador)
 
-    def actualizar_jugador(self, id_jugador, nombre_jugador, puntaje):
-        return self.__jugadordao.actualizar_jugador(id_jugador, nombre_jugador, puntaje)
+    def agregar_jugador(self, jugador):
+        return self.__jugadordao.agregar_jugador(jugador)
+
+
+    def actualizar_jugador(self, jugador):
+        return self.__jugadordao.actualizar_jugador(jugador)
 
 
     def borrar_jugador(self, id_jugador):
         return self.__jugadordao.borrar_jugador(id_jugador)
+    
+    #GETTERS Y SETTERS
+    def get_id_jugador (self):
+        return self.__id_jugador
+    def get_nombre_jugador(self):
+        return self.__nombre_jugador
+    def get_puntaje(self):
+        return self.__puntaje
+    def get_estado_jugador(self):
+        return self.__estado_jugador
+    def set_id_jugador (self, id_jugador):
+        self.__id_jugador = id_jugador
+    def set_nombre_jugador (self, nombre_jugador):
+        self.__nombre_jugador = nombre_jugador
+    def set_puntaje (self,puntaje):
+        self.__puntaje = puntaje
+    def set_estado_jugador (self, estado_jugador):
+        self.__estado_jugador = estado_jugador
