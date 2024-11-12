a4a08b7fd4ea93ff5b20ca2b67a7c9ed32e5db96
DayanaLaime <dayanalaime91@gmail.com>
2024-11-12 15:51:55 -0300

    cambios

--- controlador/ControladorVistaJugadorNuevo.py
+++ controlador/ControladorVistaJugadorNuevo.py
@@ -1,6 +1,6 @@
 from vista.VistaJugadorNuevo import Ui_MainWindow
 from PyQt6 import QtWidgets
-from Modelo.Jugador import Jugador
+#from Modelo.Jugador import Jugador
 
 class ControladorVistaJugadorNuevo:
 
@@ -11,7 +11,7 @@ class ControladorVistaJugadorNuevo:
         self.__vista.setupUi(self.MainWindow)
         self.MainWindow.show()
         
-        self.__vista.get_button_agregar_jugador().clicked.connect(self.__agregar_jugador_bd_)
+        #self.__vista.get_button_agregar_jugador().clicked.connect(self.__agregar_jugador_bd_)
         self.__vista.get_button_cancelar().clicked.connect(self.__volver_seleccion_de_jugadores)
         #self.__vista.get_boton_deslizador_derecha().clicked.connect(self.__mostrar_siguiente_imagen)
         #self.__vista.get_boton_deslizador_izquierda().clicked.connect(self.__mostrar_imagen_anterior)
@@ -19,7 +19,8 @@ class ControladorVistaJugadorNuevo:
     def __volver_seleccion_de_jugadores(self):
         self.MainWindow.close()
         self.__controlador_anterior.MainWindow.show()
-        
+    
+        '''    
     def __agregar_jugador_bd_(self):
         #Agrega un nuevo jugador y su avatar a la BD,Validandolos
         try:
@@ -34,14 +35,10 @@ class ControladorVistaJugadorNuevo:
                 self.MainWindow.close()
         except:
                 self.__vista.imprimo_alerta()
-    '''            
+        '''            
     def __mostrar_siguiente_imagen(self):
         pass
     
     
     def __mostrar_imagen_anterior(self):
         pass
-        
-    '''
-        
-    
