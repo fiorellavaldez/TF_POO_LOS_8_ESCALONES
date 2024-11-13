28cada03f5847d2b136cfb2e0ec0e1ba35efe487
kadiruriel <kadiruriel@gmail.com>
2024-11-13 19:43:57 -0300

    cambios connection JugadorDAO.py

--- Modelo/JugadorDAO.py
+++ Modelo/JugadorDAO.py
@@ -8,17 +8,17 @@ class JugadorDAO:
     
 
     def get_all_jugadores(self):
-        with self.connection.cursor() as cursor:
+        with self.__bd.cursor() as cursor:
             cursor.execute("SELECT nombre_jugador, puntaje FROM jugador")
             return cursor.fetchall()
 
     def get_jugador(self, id_jugador):
-        with self.connection.cursor() as cursor:
+        with self.__bd.cursor() as cursor:
             cursor.execute("SELECT nombre_jugador, puntaje FROM jugador WHERE id_jugador = %s", (id_jugador,))
             return cursor.fetchone()
 
     def agregar_jugador(self, jugador):
-        with self.connection.cursor() as cursor:
+        with self.__bd.cursor() as cursor:
             cursor.execute(
                 """INSERT INTO jugador (id_jugador, nombre_jugador, puntaje , estado_jugador) 
                 VALUES (%s, %s, %s, %s)""",
@@ -26,19 +26,19 @@ class JugadorDAO:
             )
             indice_jugador = cursor.execute ("SELECT MAX(id_jugador) FROM jugador ")
             jugador.set_id_jugador (int(indice_jugador)) #esto lo que hace es incrementar el indice porque los id son tipo serial
-            self.connection.commit()
-            self.connection.commit()
+            self.__bd.commit()
+
 
     def actualizar_jugador(self, jugador):
-        with self.connection.cursor() as cursor:
+        with self.__bd.cursor() as cursor:
             cursor.execute(
                 """UPDATE jugador SET nombre_jugador = %s, puntaje = %s 
                 WHERE id_jugador = %s""",
                 (jugador.get_nombre_jugador(), jugador.get_puntaje(), jugador.get_id_jugador())
             )
-            self.connection.commit()
+            self.__bd.commit()
 
     def borrar_jugador(self, id_jugador):
-        with self.connection.cursor() as cursor:
+        with self.__bd.cursor() as cursor:
             cursor.execute("UPDATE jugador SET estado_jugador FALSE WHERE id_jugador = %s", (id_jugador,))
-            self.connection.commit()
+            self.__bd.commit()
