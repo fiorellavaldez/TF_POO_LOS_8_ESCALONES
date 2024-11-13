f0b7895558c9fe7396c435bb8bfa3b1c3728cc01
Fiore <fiorellavald3z@gmail.com>
2024-11-13 10:07:39 -0300

    borré un comentario

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     controlador = ControladorPantallaInicio()
-    sys.exit(app.exec()) #hola
+    sys.exit(app.exec())
 
